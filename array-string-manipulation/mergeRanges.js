7f247c2c01769de8dc9af3820312e0e4b21bff8d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-29 18:50:06 -0800

    edge case of there only being one meeting in array handled

--- array-string-manipulation/mergeRanges.js
+++ array-string-manipulation/mergeRanges.js
@@ -3,10 +3,23 @@
  */
 
 const mergeRanges = (arr) => {
+  //we probably dont want to exert any effort if there isnt more than one meeting
+  if (arr.length < 2) {
+    return arr
+  }
   //we should sort the arr by the startTimes. In order to do that we must pass in a comparison func to sort
   arr.sort((a, b) => {
     return a.startTime - b.startTime
   })
+
+  //now that the meetings are sorted, we want to check if meeting times overlap.
+  //we need to set a the first meeting as the start
+  // let start = arr[0];
+  //then we have to loop the remaing meetings, they overlap if the first if endtime of current meeting is gt starttime of currentMeeting + 1
+  for (let i = 0; i <= arr.length; i++) {
+    let currentMeeting = arr[i];
+    if ()
+  }
 
   return arr
 }
