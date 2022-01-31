558da9ddaee6ddbdbb3a91b63d984d373cc28fb9
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-31 08:40:03 -0800

    made deep copy of given array and sorted it by start time

--- array-string-manipulation/mergeRanges.js
+++ array-string-manipulation/mergeRanges.js
@@ -3,25 +3,41 @@
  */
 
 const mergeRanges = (arr) => {
-  //we probably dont want to exert any effort if there isnt more than one meeting
-  if (arr.length < 2) {
-    return arr
-  }
-  //we should sort the arr by the startTimes. In order to do that we must pass in a comparison func to sort
-  arr.sort((a, b) => {
-    return a.startTime - b.startTime
-  })
+  // create deep clone of the meetings array
+  const meetingsCopy = JSON.parse(JSON.stringify(arr))
 
-  //now that the meetings are sorted, we want to check if meeting times overlap.
-  //we need to set a the first meeting as the start
-  // let start = arr[0];
-  //then we have to loop the remaing meetings, they overlap if the first if endtime of current meeting is gt starttime of currentMeeting + 1
-  for (let i = 0; i <= arr.length; i++) {
-    let currentMeeting = arr[i];
-    if ()
-  }
+  console.log(meetingsCopy)
+  // //we probably dont want to exert any effort if there isnt more than one meeting
+  // if (arr.length < 2) {
+  //   return arr
+  // }
+  // //we should sort the arr by the startTimes. In order to do that we must pass in a comparison func to sort
+  // arr.sort((a, b) => {
+  //   return a.startTime - b.startTime
+  // })
 
-  return arr
+  // //now that the meetings are sorted, we want to check if meeting times overlap.
+  // //we need to set a the lastMeeting to equal the last item in meetings arr
+  // // let lastMeeting = arr[arr.length - 1]
+  // //then we have to loop the remaing meetings, they overlap if the first if endtime of current meeting is gt starttime of currentMeeting + 1
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   let currentMeeting = arr[i]
+  //   let nextMeeting = arr[i+1]
+
+  //   console.log('hello')
+
+  //   if (nextMeeting) {
+  //         if (currentMeeting.endTime >= nextMeeting.startTime) {
+  //           currentMeeting.endTime = nextMeeting.endTime
+  //            return arr.slice(next, arr[i+2])
+  //         }
+
+  //   }
+
+  // }
+
+  // return arr
 }
 
 exampleArr = [
